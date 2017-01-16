3d7aab5394490d755934da57f8b7fe1135123176
gabriela <gabdelc@gmail.com>
2017-01-16 10:34:37 -0500

    reemplazo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-document.getElementsById("inputEmail")[0].placeholder="Correo electronico";
-document.getElementsById("inputPassword")[0].placeholder="Contraseña";
 
-function replaceText(selector, text, newText, flags) {
-  var matcher = new RegExp(text, flags);
-  $(selector).each(function () {
-    var $this = $(this);
-    if (!$this.children().length)
-       $this.text($this.text().replace(matcher, newText));
-  });
-}
+document.getElementById("form-signin-heading").innerHTML="Por favor inicie sesión";
 
-function replaceText(){
-    
-    
-    document.getElementsById("inputEmail")[0].placeholder="Correo electronico";
-    document.getElementsById("inputPassword")[0].placeholder="Contraseña";
-}
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+
+document.getElementById("inputPassword").placeholder="Contraseña";
+
+document.querySelector("span").innerHTML="Recordar datos";
+
+document.querySelector("button").innerHTML="Iniciar Sesión"; 
+
+
